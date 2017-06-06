ba3442d5dca34a4ce59964bcb1e647c1760ddb67
SL <uaglc@student.kit.edu>
2017-06-06 17:57:05 +0200

    1225 Neuer branch

--- src/main.java
+++ src/main.java
@@ -5,6 +5,7 @@ public class main {
     public static void main(String[] args){
         System.out.println("Hallo");
         System.out.println("lala");
+        System.out.println("Neuer Branch");
     }
 
 }
